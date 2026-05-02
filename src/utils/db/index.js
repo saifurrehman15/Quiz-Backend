c507d812e4d1f9ef0fb3597ba09b1ae504f78549
unknown <saifrizwankhan786@gmail.com>
2026-05-02 21:36:58 +0500

    update

--- src/utils/db/index.js
+++ src/utils/db/index.js
@@ -1,12 +1,18 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(
-    process.env.dbName,
-    process.env.user,
-    process.env.dbpassword,
-    {
-        host: 'localhost',
-        dialect: process.env.database,
-        logging: false,
-    }
-);
+// export const sequelize = new Sequelize(
+//     process.env.dbName,
+//     process.env.user,
+//     process.env.dbpassword,
+//     {
+//         host: 'localhost',
+//         dialect: process.env.database,
+//         logging: false,
+//     }
+// );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+});
