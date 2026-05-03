c600899118542356053cfd09eb7066076cf3801a
unknown <saifrizwankhan786@gmail.com>
2026-05-04 03:38:51 +0500

    added

--- src/utils/db/index.js
+++ src/utils/db/index.js
@@ -1,4 +1,4 @@
-// import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize";
 
 // export const sequelize = new Sequelize(
 //     process.env.dbName,
@@ -18,7 +18,7 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, // 👈 VERY IMPORTANT
+      rejectUnauthorized: false,
     },
   },
 });
