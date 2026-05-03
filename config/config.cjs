3dbb7e89e035a271f3adbb9f312d1dcfced76a28
unknown <saifrizwankhan786@gmail.com>
2026-05-04 03:57:57 +0500

    added

--- config/config.cjs
+++ config/config.cjs
@@ -2,26 +2,16 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    use_env_variable: "DATABASE_URL",
+    url: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-      keepAlive: true,
+      ssl: false,  // SSL disable karo for local
     },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 60000,
-      idle: 10000,
-    },
-    logging: false,
+    logging: console.log,
   },
 
   production: {
-    use_env_variable: "DATABASE_URL",
+    url: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
